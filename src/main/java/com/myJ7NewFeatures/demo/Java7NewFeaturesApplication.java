93d6f89b07b98d3dc9e4a25d8d12a9f0281884c7
Colin O'Hanlon <coohanlonCode@google.com>
2020-06-18 11:54:14 -0400

    first commit

---
+++ src/main/java/com/myJ7NewFeatures/demo/Java7NewFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.myJ7NewFeatures.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java7NewFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java7NewFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myJ7NewFeatures/demo/Java7NewFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myJ7NewFeatures.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java7NewFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
